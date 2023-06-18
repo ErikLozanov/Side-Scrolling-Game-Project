4a9c9186790d4fd3f5b0647b273d797d844e8158
ErikLozanov <wolfierecordss@gmail.com>
2023-06-18 16:50:08 +0300

    Added Press To Play click and hide.

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+// select game screens
+
+const gameStart = document.querySelector('.game-start');
+const gameArea = document.querySelector('.game-area');
+const gameOver = document.querySelector('.game-over');
+const gameScore = document.querySelector('.game-score');
+
+
+gameStart.addEventListener('click', onGameStart);
+
+
+function onGameStart() {
+    gameStart.classList.add('hide');
+    console.log('hi!');
+}
