7bbc025640389a19ef5ac31fcc82835b75a4ef41
ErikLozanov <wolfierecordss@gmail.com>
2023-06-19 13:08:58 +0300

    Added moving clouds

--- src/js/main.js
+++ src/js/main.js
@@ -45,20 +45,28 @@ let player = {
     x: Math.floor(Math.random() * 100),
     y: Math.floor(Math.random() * 100),
     width: 0,
-    height: 0
+    height: 0,
+    lastFiredFireball: 0,
 };
 let game = {
     speed: 2,
-    movingMultiplier: 4
+    movingMultiplier: 2,
+    fireBallMultiplier: 5,
+    fireRate: 1000,
+    cloudSpawningRate: 3000,
 };
 let scene = {
-    score: 0
+    score: 0,
+    lastSpawnedCloud: 0
+
 }
+
 
 // game actions
 
-function gameAction() {
+function gameAction(timestamp) {
     const wizardElement = document.querySelector('.wizard');
+    const clouds = document.createElement('div');
 
     // Borders
     if(keys.KeyW && player.y > 0) {
@@ -74,11 +82,29 @@ function gameAction() {
         player.x += game.speed * game.movingMultiplier;
     }
 
+    // Clouds
+    if(timestamp - scene.lastSpawnedCloud > game.cloudSpawningRate) {
+        clouds.classList.add('clouds');
+        addClouds();
+        scene.lastSpawnedCloud = timestamp;
+    } else {
+       clouds.classList.remove('clouds') 
+    }
+    // Modify moving clouds
+    let allClouds = document.querySelectorAll('.clouds');
+    allClouds.forEach(cloud => {
+        cloud.x -= game.speed;
+        cloud.style.left = cloud.x + 'px';
+
+        if(cloud.x + clouds.offsetWidth <= 0) {
+            cloud.parentElement.removeChild(cloud);
+        }
+    })
     // Fireball Shooting
-    if(keys.Space) {
+    if(keys.Space && timestamp - player.lastFiredFireball > game.fireRate) {
         wizardElement.classList.add('wizard-fire');
         addFireBall();
-
+        player.lastFiredFireball = timestamp;
     } else {
         wizardElement.classList.remove('wizard-fire');
     }
@@ -119,4 +145,13 @@ function addFireBall() {
     fireBall.x = player.x + player.width;
     fireBall.style.left = fireBall.x + 'px';
     gameArea.appendChild(fireBall);
+}
+
+function addClouds() {
+    let cloud = document.createElement('div');
+    cloud.classList.add('clouds');
+    cloud.style.top = Math.floor(Math.random() * 100) + 'px';
+    cloud.x = gameArea.offsetWidth;
+    cloud.style.left = cloud.x + 'px';
+    gameArea.appendChild(cloud);
 }
